92dc247c571be19a3899cf407310b9cf78e33706
Matias Kruk <kruk.matias@gmail.com>
2020-04-15 17:49:10 +0200

    worldometer

--- worldometer/updater-kaggle.py
+++ worldometer/updater-kaggle.py
@@ -6,9 +6,10 @@ import kaggle
 import subprocess
 
 def job():
-    print("Extract data")
+    print("Extract data")    
+    subprocess.call(['scrapy', 'crawl', 'worldometer', '-o', 'wworldometer.csv','-t','csv'])
     subprocess.call(['kaggle', 'datasets', 'version', '-p', '.', '-m', 'new version'])
-    
+
 schedule.every().day.at("17:00").do(job)
 
 while True:
