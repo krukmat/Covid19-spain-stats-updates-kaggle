510fcd9e315ed078a765df98751171ca178ea43e
Matias Kruk <kruk.matias@gmail.com>
2020-04-15 17:46:53 +0200

    updater

---
+++ worldometer/updater-kaggle.py
@@ -0,0 +1,16 @@
+import schedule
+import time
+import urllib2
+import csv 
+import kaggle
+import subprocess
+
+def job():
+    print("Extract data")
+    subprocess.call(['kaggle', 'datasets', 'version', '-p', '.', '-m', 'new version'])
+    
+schedule.every().day.at("17:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
