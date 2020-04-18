a6f576a9ac90ea1563eaeaf35dea5107c4b2e153
Matias Kruk <kruk.matias@gmail.com>
2020-04-18 18:31:49 +0200

    heroku push

---
+++ update-heroku.py
@@ -0,0 +1,15 @@
+import schedule
+import time
+import requests
+
+def job():
+    url = 'https://worldometer-scrapper.herokuapp.com/api/update/'
+    print("send data to heroku")    
+    files = {'file': open('/root/worldometer/dataset/wworldometer.csv', 'rb')}
+    r = requests.post(url, files=files)
+
+schedule.every().day.at("22:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
