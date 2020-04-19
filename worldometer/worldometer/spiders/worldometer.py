6aa3e1cfa2d2a7abee66d1d4666ec526c2ca52b4
Matias Kruk <kruk.matias@gmail.com>
2020-04-19 21:15:33 +0200

    remove non latin chars

--- worldometer/worldometer/spiders/worldometer.py
+++ worldometer/worldometer/spiders/worldometer.py
@@ -8,7 +8,7 @@ class WorldometerSpider(scrapy.Spider):
     ]
 
     def _removeNonAscii(self, s): 
-        return "".join(i for i in s if ord(i)<128)
+        return "".join(i for i in s if ord(i)<128) if s != None and s!= '' else ''
 
     def evaluate_col(self, country, index):
         return country.css('td')[index].css('td::text').get()!=None and (country.css('td')[index].css('td::text').get()!=None and  country.css('td')[index].css('td::text').get().strip() != 'N/A' and country.css('td')[index].css('td::text').get().strip() !='')
@@ -16,7 +16,7 @@ class WorldometerSpider(scrapy.Spider):
     def parse(self, response):
         for country in response.xpath('//*[@id="main_table_countries_today"]/tbody[1]/tr[@style=""]'):
             yield {
-                'name': self._removeNonAscii(country.css('td a::text').get()),
+                'name': self._removeNonAscii(country.css('td a::text').get()) , 
                 'totalcases': int(country.css('td')[1].css('td::text').get().replace('"','').replace(',','')) if  self.evaluate_col(country, 1)  else 0,
                 'newcases':int(country.css('td')[2].css('td::text').get().replace('"','').replace(',','')) if self.evaluate_col(country, 2) else 0,
                 'totaldeath':int(country.css('td')[3].css('td::text').get().replace('"','').replace(',','')) if self.evaluate_col(country, 3)  else 0,
