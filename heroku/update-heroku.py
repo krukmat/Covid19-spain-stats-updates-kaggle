5fda859c17c4ed2c824f4ffb7f27b5bdf21f3773
Matias Kruk <kruk.matias@gmail.com>
2020-04-19 21:20:43 +0200

    tsk

--- heroku/update-heroku.py
+++ heroku/update-heroku.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 def job():
-    url = 'http://134.122.68.234/api/update/'
+    url = 'http://127.0.0.1:8000/api/update/'
     r = requests.post(url)
 
 schedule.every().day.at("18:00").do(job)
