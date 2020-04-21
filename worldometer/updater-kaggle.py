3b1a4a66a85faa28214bf2a28b3890bc281fc6c1
Matias Kruk <kruk.matias@gmail.com>
2020-04-21 19:38:01 +0200

    time

--- worldometer/updater-kaggle.py
+++ worldometer/updater-kaggle.py
@@ -11,7 +11,7 @@ def job():
     subprocess.call(['scrapy', 'crawl', 'worldometer', '-o', '/root/worldometer/dataset/wworldometer.csv','-t','csv'])
     subprocess.call(['kaggle', 'datasets', 'version', '-p', '/root/worldometer/dataset/', '-m', 'new version'])
 
-schedule.every().day.at("17:58").do(job)
+schedule.every().day.at("17:50").do(job)
 
 while True:
     schedule.run_pending()
